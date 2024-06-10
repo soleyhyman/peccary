ec5bc8d94ea75041d35d6fb0f2ce309c879ed89a
soleyhyman <soleyhyman@arizona.edu>
2024-06-10 14:16:08 -0700

    added module description, doublePendulum class

--- peccary/timeseries.py
+++ peccary/timeseries.py
@@ -1,12 +1,12 @@
-
-# """Timeseries functions for testing PECCARY"""
-# __all__ = [
-#     "lorenz",
-#     "lorenz_mod",
-#     "generateLorenz"
-# ]
+"""
+The functions and classes in the ``timeseries`` module consist of
+different functions and physical systems used for testing PECCARY
+"""
 
 import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 def generateHenon(n, a=1.4, b=0.3):
     """
@@ -182,3 +182,178 @@ class lorenz:
             xyzs[i + 1] = xyzs[i] + self.getPartials(xyzs[i]) * dt
 
         return xyzs.T[0], xyzs.T[1], xyzs.T[2]
+    
+class doublePendulum:
+    def __init__(self, t_stop=2.5, dt=np.power(2.,-6.), L1=1.0, L2=1.0, M1=1.0, M2=1.0):
+        """
+        Initialize double pendulum function
+
+        Notes
+        -----
+        Code based on a `Matplotlib tutorial<https://matplotlib.org/stable/gallery/animation/double_pendulum.html>`_
+        and translated into a Python class by Sóley Hyman.
+        The formulae in that tutorial turn were translated from the C code by `Michael S. Wheatland<http://www.physics.usyd.edu.au/~wheat/dpend_html/solve_dpend.c>`_
+
+        Parameters
+        ----------
+        t_stop : float, optional
+            How many seconds to simulate, by default 2.5
+        dt : float, optional
+            Time resolution in seconds, by default 0.015625
+        L1 : float, optional
+            Length of pendulum 1 in m, by default 1.0
+        L2 : float, optional
+            Length of pendulum 2 in m, by default 1.0
+        M1 : float, optional
+            Mass of pendulum 1 in kg, by default 1.0
+        M2 : float, optional
+            Mass of pendulum 2 in kg, by default 1.0
+        """
+        self.t_stop = t_stop
+        self.dt = dt
+        self.L1 = L1
+        self.L2 = L2
+        self.L = L1 + L2
+        self.M1 = M1
+        self.M2 = M2
+        self.g = 9.80665 # m/s^2
+
+        # create a time array from 0 to t_stop, sampled at intervals of dt
+        self.t = np.arange(0, self.t_stop, self.dt)
+
+    def derivs(self, t, state):
+        """
+        Get partial derivatives for double pendulum system
+
+        Parameters
+        ----------
+        t : ndarray
+            Time array
+        state : _type_
+            Initial conditions
+
+        Returns
+        -------
+        ndarray
+            Partial derivatives
+        """
+        dydx = np.zeros_like(state)
+
+        dydx[0] = state[1]
+
+        delta = state[2] - state[0]
+        den1 = (self.M1+self.M2) * self.L1 - self.M2 * self.L1 * np.cos(delta) * np.cos(delta)
+        dydx[1] = ((self.M2 * self.L1 * state[1] * state[1] * np.sin(delta) * np.cos(delta)
+                    + self.M2 * self.g * np.sin(state[2]) * np.cos(delta)
+                    + self.M2 * self.L2 * state[3] * state[3] * np.sin(delta)
+                    - (self.M1+self.M2) * self.g * np.sin(state[0]))
+                / den1)
+
+        dydx[2] = state[3]
+
+        den2 = (self.L2/self.L1) * den1
+        dydx[3] = ((- self.M2 * self.L2 * state[3] * state[3] * np.sin(delta) * np.cos(delta)
+                    + (self.M1+self.M2) * self.g * np.sin(state[0]) * np.cos(delta)
+                    - (self.M1+self.M2) * self.L1 * state[1] * state[1] * np.sin(delta)
+                    - (self.M1+self.M2) * self.g * np.sin(state[2]))
+                / den2)
+
+        return dydx
+    
+    def integrateSim(self, th1=120.0, w1=0.0, th2=-10.0, w2=0.0):
+        """
+        Integrate double pendulum system
+
+        Parameters
+        ----------
+        th1 : float, optional
+            Initial angle of mass 1 in degrees, by default 120.0*u.deg
+        w1 : float, optional
+            Initial angular velocity of mass 1 in degrees/second, by default 0.0
+        th2 : float, optional
+            Initial angle of mass 2 in degrees, by default -10.0*u.deg
+        w2 : float, optional
+            Initial angular velocity of mass 2 in degrees/second, by default 0.0
+        """
+        # initial state
+        state = np.radians([th1, w1, th2, w2])
+
+        y = solve_ivp(self.derivs, self.t[[0, -1]], state, t_eval=self.t).y.T
+
+        self.x1 = self.L1*np.sin(y[:, 0])
+        self.y1 = -self.L1*np.cos(y[:, 0])
+
+        self.x2 = self.L2*np.sin(y[:, 2]) + self.x1
+        self.y2 = -self.L2*np.cos(y[:, 2]) + self.y1
+
+    def plotStatic(self):
+        """
+        Quick-plot y(x), x(t), and y(t) for the second mass (lower mass)
+        of the double pendulum system.
+        """
+        fig,axs = plt.subplots(1,3, figsize=(15, 4))
+        axs[0].set_aspect('equal')
+        axs[0].plot(self.x2,self.y2)
+        axs[0].set_title('XY plane of pendulum movment')
+
+        axs[1].plot(self.t,self.x2)
+        axs[1].set_xlabel('Time (seconds)')
+        axs[1].set_title('X coordinate')
+
+        axs[2].plot(self.t,self.y2)
+        axs[2].set_xlabel('Time (seconds)')
+        axs[2].set_title('Y coordinate')
+
+        return fig,axs
+    
+    def animateFrame(self, i):
+        """
+        Animate a frame of the double pendulum system
+
+        Notes
+        -----
+        This will very rarely be used alone; please use 
+        the plotAnimate function instead, which is a wrapper
+        for this function        
+
+        Parameters
+        ----------
+        i : int
+            Index of the frame to animate
+
+        Returns
+        -------
+        Matplotlib Line2D object
+            Lines representing the pendulum rods
+        Matplotlib Line2D object
+            Points representing history 
+        Matplotlib Text object
+            Text object of current frame timestep
+        """
+        presentX = [0, self.x1[i], self.x2[i]]
+        presentY = [0, self.y1[i], self.y2[i]]
+
+        pastX = self.x2[:i]
+        pastY = self.y2[:i]
+
+        self.line.set_data(presentX, presentY)
+        self.trace.set_data(pastX, pastY)
+        self.textCurrentTime.set_text(self.time_template % (i*self.dt))
+        return self.line, self.trace, self.textCurrentTime
+
+    def plotAnimate(self):
+        """
+        Animate the integrated double pendulum system
+        """
+        fig = plt.figure(figsize=(5, 4))
+        ax = fig.add_subplot(autoscale_on=False, xlim=(-self.L, self.L), ylim=(-self.L, 1.))
+        ax.set_aspect('equal')
+        ax.grid()
+
+        self.line, = ax.plot([], [], 'o-', lw=2)
+        self.trace, = ax.plot([], [], '.-', lw=1, ms=2)
+        self.time_template = 'time = %.1fs'
+        self.textCurrentTime = ax.text(0.05, 0.9, '', transform=ax.transAxes)
+        
+        ani = FuncAnimation(fig, self.animateFrame, len(self.y2), interval=self.dt*1000, blit=True)
+        plt.show()
