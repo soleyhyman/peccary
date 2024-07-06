7734ebd93bcfd17e6c8ffefcccb158682810d978
soleyhyman <soleyhyman@arizona.edu>
2024-07-05 18:26:34 -0700

    changed embedding dimension/delay terms to sampling size/intervals

--- peccary/peccary.py
+++ peccary/peccary.py
@@ -21,7 +21,7 @@ class peccary:
         data : 1-D array
             Timeseries data for PECCARY 
         n : int, optional
-            Embedding dimension, by default 5
+            Sampling size, by default 5
 
         Attributes
         ----------
@@ -59,68 +59,68 @@ class peccary:
         self.log2_N = np.log2(self.N) # log_2(n!)
         self.log2_Np1 = np.log2(self.N+1.) # log_2(n! + 1)
 
-    def tPattern(self, dt, delay=1):
+    def tPat(self, dt, sampInt=1):
         """
         Calculates pattern time of in PECCARY routine, based on 
-        embedding dimension and embed delay
+        sampling size and sampling interval
 
         Parameters
         ----------
         dt : float
             Timestep interval
-        delay : int or ndarray, optional
-            Embed delay/pattern length, by default 1
+        sampInt : int or ndarray, optional
+            Sampling interval, by default 1
 
         Returns
         -------
         float or ndarray
-            Pattern time(s) corresponding to inputted embed delay(s)
+            Pattern time(s) corresponding to inputted sampling interval(s)
         """
-        return delay * dt * (self.n - 1.)
+        return sampInt * dt * (self.n - 1.)
     
-    def tau_from_tPattern(self, dt, tPattern):
+    def ell_from_tPat(self, dt, tPat):
         """
-        Calculates the embed delay in PECCARY routine based 
-        on the pattern time and embedding dimension
+        Calculates the sampling interval in PECCARY routine based 
+        on the pattern time and sampling size
 
         Parameters
         ----------
         dt : float
             Timestep interval
-        tPattern : float or ndarray
+        tPat : float or ndarray
             Pattern time
 
         Returns
         -------
         float or ndarray
-            Embed delay(s) corresponding to inputted pattern time(s)
+            Sampling interval(s) corresponding to inputted pattern time(s)
         """
-        return tPattern/(dt * (self.n - 1.))
+        return tPat/(dt * (self.n - 1.))
     
-    def getPtot(self,delay=1):
+    def getPtot(self,sampInt=1):
         """
-        Get total number of permutations based on embed delay
+        Get total number of permutations based on sampling interval
 
         Parameters
         ----------
-        delay : int, optional
-            Embed delay/pattern length, by default 1
+        sampInt : int, optional
+            Sampling interval, by default 1
 
         Returns
         -------
         int
             Total number of permutations
         """
-        return self.t - delay*(self.n - 1) # Total number of order n permutations in T; Weck+2015 Eq.(1) denominator
+        return self.t - sampInt*(self.n - 1) # Total number of order n permutations in T; Weck+2015 Eq.(1) denominator
 
-    def constructPatternCount(self,delay=1):
+    def constructPatternCount(self,sampInt=1):
         """
         Count occurance of patterns and total number of permutations
 
         Parameters
         ----------
-        delay : int, optional
-            Embed delay, by default 1
+        sampInt : int, optional
+            Sampling interval, by default 1
 
         Returns
         -------
@@ -129,21 +129,21 @@ class peccary:
         Ptot : int 
             Total number of permutations
         """
-        Ptot = self.t - delay*(self.n - 1) # Total number of order n permutations in T; Weck+2015 Eq.(1) denominator
+        Ptot = self.t - sampInt*(self.n - 1) # Total number of order n permutations in T; Weck+2015 Eq.(1) denominator
         
-        patterns = np.array([self.T[i:i+(self.n-1)*delay+1:delay].argsort(kind='stable') for i in range(Ptot)]) # Get patterns based on n and delay (aka tau)
+        patterns = np.array([self.T[i:i+(self.n-1)*sampInt+1:sampInt].argsort(kind='stable') for i in range(Ptot)]) # Get patterns based on sampling size and interval
         count = np.unique(patterns, axis=0, return_counts=True) # Count the number of unique ordinal patterns
         
         return count, Ptot
 
-    def getPatternsCounts(self,delay=1):
+    def getPatternsCounts(self,sampInt=1):
         """
         Return patterns, respective counts, and total number of permutations
 
         Parameters
         ----------
-        delay : int, optional
-            Embed delay, by default 1
+        sampInt : int, optional
+            Sampling interval, by default 1
 
         Returns
         -------
@@ -154,21 +154,21 @@ class peccary:
         Ptot : int 
             Total number of permutations
         """
-        Ptot = self.t - delay*(self.n - 1) # Total number of order n permutations in T; Weck+2015 Eq.(1) denominator
+        Ptot = self.t - sampInt*(self.n - 1) # Total number of order n permutations in T; Weck+2015 Eq.(1) denominator
         
-        patterns = np.array([self.T[i:i+(self.n-1)*delay+1:delay].argsort(kind='stable') for i in range(Ptot)]) # Get patterns based on n and delay (aka tau)
+        patterns = np.array([self.T[i:i+(self.n-1)*sampInt+1:sampInt].argsort(kind='stable') for i in range(Ptot)]) # Get patterns based on sampling size and interval
         count = np.unique(patterns, axis=0, return_counts=True) # Count the number of unique ordinal patterns
         
-        return count[0],count[1], Ptot
+        return count[0], count[1], Ptot
     
-    def calcS(self, delay=1):		
+    def calcS(self, sampInt=1):		
         """
         Calculate Shannon permutation entropy (S) and disqeulibrium (S_e)
 
         Parameters
         ----------
-        delay : int, optional
-            Integer delay, by default 1
+        sampInt : int, optional
+            Sampling interval, by default 1
 
         Returns
         -------
@@ -177,7 +177,7 @@ class peccary:
         Se : float
             Disequilibrium
         """
-        count,Ptot = self.constructPatternCount(delay) # Get pattern counts and total number of permutations
+        count,Ptot = self.constructPatternCount(sampInt) # Get pattern counts and total number of permutations
         invPtot=1./Ptot # Inverse of total number of permutations
 
         #Calculate S from the count
@@ -186,14 +186,14 @@ class peccary:
         Se = -1.*np.sum([P_Pe_sum_2*np.log2(P_Pe_sum_2) for P_Pe_sum_2 in 0.5*(probabilities+self.invN)]) + 0.5*(self.N-len(probabilities))*self.invN*(1+self.log2_N) # Disequilibrium between distribution and uniform distribution; Schaffner & Daniel (in prep), Eq.(8)
         return S, Se
 
-    def calcH(self,delay=1):		
+    def calcH(self,sampInt=1):		
         """
         Calculate normalized Permutation Entropy
 
         Parameters
         ----------
-        delay : int, optional
-            Integer delay, by default 1
+        sampInt : int, optional
+            Sampling interval, by default 1
 
         Returns
         -------
@@ -202,18 +202,18 @@ class peccary:
         Se : float
             Normalized Shannon + Uniform permutation entropy
         """
-        S, Se = self.calcS(delay) # Shannon entropy and disequilibrium
+        S, Se = self.calcS(sampInt) # Shannon entropy and disequilibrium
         return S/self.log2_N,Se/self.log2_N 
 
-    def calcCofH(self,delay=1):
+    def calcCofH(self,sampInt=1):
         """
         Calculate normalized Jensen-Shannon statistical complexity 
         and normalized permutation entropy
 
         Parameters
         ----------
-        delay : int, optional
-            Integer delay, by default 1
+        sampInt : int, optional
+            Sampling interval, by default 1
 
         Returns
         -------
@@ -222,7 +222,7 @@ class peccary:
         C : float
             Normalized Jensen-Shannon statistical complexity
         """
-        S, Se  = self.calcS(delay) # Shannon entropy and disequilibrium
+        S, Se  = self.calcS(sampInt) # Shannon entropy and disequilibrium
         H = S/self.log2_N # Normalized permutation entropy; Schaffner & Daniel (in prep) Eq.(3)
         C = -2.*((Se - 0.5*S - 0.5*self.log2_N)
                 /((1. + self.invN)*self.log2_Np1 - 2.*np.log2(2.*self.N) 
@@ -271,37 +271,37 @@ class peccary:
         Se = -1.*np.sum([P_Pe_sum_2*np.log2(P_Pe_sum_2) for P_Pe_sum_2 in 0.5*(probabilities+self.invN)]) + 0.5*(self.N-len(probabilities))*self.invN*(1+self.log2_N) # Disequilibrium between distribution and uniform distribution; Schaffner & Daniel (in prep), Eq.(8)
         return S,Se
 
-    def calcPESCcurves(self, min_delay=1, max_delay=100, delayInt=1, delayArray=None):
+    def calcPESCcurves(self, min_sampInt=1, max_sampInt=100, step_sampInt=1, sampIntArray=None):
         """
-        Returns PE(tau) and SC(tau) for specified range of tau values
+        Returns Permutation Entropy and Statistical Complexity values for multiple
+        sampling interval values, i.e., :math:`H(\ell)` and :math:`C(\ell) `
 
         Parameters
         ----------
         data : array
             Timeseries data for PECCARY
         n : int, optional
-            Embedding dimension, by default 5
-        min_delay : int, optional
-            Smallest embedding delay to loop through, by default 1
-        max_delay : int, optional
-            Largest embedding delay to loop through, by default 100
-        delayInt : int, optional
+            Sampling size, by default 5
+        min_sampInt : int, optional
+            Smallest sampling interval to loop through, by default 1
+        max_sampInt : int, optional
+            Largest sampling interval to loop through, by default 100
+        step_sampInt : int, optional
             How many sampling interval values to skip over
-        delayArray: ndarray or list
-            Custom array of sampling intervals, supersedes min_delay,
-            max_delay, and delayInt, by default None
-
+        sampIntArray: ndarray or list
+            Custom array of sampling intervals, supersedes min_sampInt,
+            max_sampInt, and step_sampInt, by default None
 
         Returns
         -------
-        H : array
-            Normalized Shannon Perumation Entropy as function of embedding delay tau
-        C(tau) : array
-            Normalized Jensen-Shannon complexity as function of embedding delay tau
+        :math:`H(\ell)` : array
+            Normalized Shannon Perumation Entropy as function of sampling interval :math:`\ell`
+        :math:`C(\ell)` : array
+            Normalized Jensen-Shannon complexity as function of sampling interval :math:`\ell`
         """
-        if type(delayArray) != type(None):
-            delays = delayArray # Array of sampling intervals from delayArray
+        if type(sampIntArray) != type(None):
+            sampInts = sampIntArray # Array of sampling intervals from sampIntArray
         else:
-            delays = np.arange(min_delay,max_delay,delayInt) # Make array of embed delay integers from min_delay to max_delay, increasing by 1
-        Hvals, Cvals = np.array([self.calcCofH(delay=int(delays[i])) for i in range(len(delays))]).T # Get normalized permutation entropy and statisical complexity for each of the embed delays
+            sampInts = np.arange(min_sampInt,max_sampInt,step_sampInt) # Make array of sampling intervals from min_sampInt to max_sampInt, increasing by 1
+        Hvals, Cvals = np.array([self.calcCofH(sampInt=int(sampInts[i])) for i in range(len(sampInts))]).T # Get normalized permutation entropy and statisical complexity for each of the sampling intervals
         return Hvals, Cvals
