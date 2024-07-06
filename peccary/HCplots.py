811e2302218bbdbaeaa9930e78333ee6b30ad60c
soleyhyman <soleyhyman@arizona.edu>
2024-07-05 18:00:43 -0700

    moved Cmaxmin and getMaxC to utils.py

--- peccary/HCplots.py
+++ peccary/HCplots.py
@@ -19,7 +19,7 @@ import utils
 __all__ = ["HCplots"]
 
 class HCplots:
-    def __init__(self, nsteps=1000, n=5):
+    def __init__(self, n=5, nsteps=1000):
         """
         Initialize HCplots class
 
@@ -33,93 +33,13 @@ class HCplots:
 
         Attributes
         ----------
-        nsteps : int
-            Number of steps used in generating HC bounding curves
         n : int
             Sampling size
-        N : int
-            Total number of possible ordinal pattern permutations (:math:`n!`)
-        invN : float
-            Probability of all ordinal patterns for uniform distribution (:math:`1/n!`)
-        log2_N : float
-            Calculated value of :math:`\log_2 (n!)`
-        log2_Np1 : float
-            Calculated value of :math:`\log_2 (n! + 1)`
-
+        nsteps : int
+            Number of steps used in generating HC bounding curves
         """
-        # Precalculate constant values for later use
         self.nsteps = nsteps
         self.n = n
-        self.N = factorial(n)
-        self.invN = 1./self.N
-        self.log2_N = np.log2(self.N)
-        self.log2_Np1 = np.log2(self.N+1.)
-
-    def Cmaxmin(self):	
-        """
-        Get maximum and minimum C(H) curves based on pattern length
-        for plotting on the HC plane
-
-        Returns
-        -------
-        Cminx
-            H values for minimum HC curve
-        Cminy
-            C values for minimum HC curve
-        Cmaxx
-            H values for maximum HC curve
-        Cmaxy
-            C values for maximum HC curve
-        """
-        # Set up blank arrays for x- and y-values of max/min curves
-        Cmaxx = np.zeros((self.N-1)*self.nsteps)
-        Cmaxy = np.zeros((self.N-1)*self.nsteps)
-        Cminx = np.zeros(self.nsteps)
-        Cminy = np.zeros(self.nsteps)
-        
-        # Calculate H and C values for minimum HC curve
-        for i in np.arange(self.nsteps):
-            pk = self.invN + i*(1.-(self.invN))/self.nsteps
-            pj = (1. - pk)/(self.N - 1.)
-            S = -pk * np.log2(pk) - (self.N - 1.) * pj * np.log2(pj)
-            qk = pk/2. + 1./(2.*self.N)
-            qj = pj/2. + 1./(2.*self.N)
-            Scom = -qk * np.log2(qk) - (self.N - 1.) * qj * np.log2(qj)
-            Cminx[i] = S / self.log2_N
-            Cminy[i] = -2. * (S/self.log2_N) * (Scom - 0.5*S - 0.5*self.log2_N)\
-            /((1 + self.invN)*self.log2_Np1 - 2*np.log2(2.*self.N) + self.log2_N)	
-            
-        # Calculate H and C values for maximum HC curve
-        for i in np.arange(1,self.N):
-            for l in np.arange(self.nsteps):
-                pk = l*(1./(self.N-i+1.))/self.nsteps
-                pj = (1. - pk)/(self.N - i)
-                if pk ==0.:
-                    S = -(self.N - i) * pj * np.log2(pj)
-                else:
-                    S = -pk * np.log2(pk) - (self.N - i) * pj * np.log2(pj)
-                qk = pk/2. + 1./(2.*self.N)
-                qj = pj/2. + 1./(2.*self.N)
-                Scom = -qk * np.log2(qk) - (self.N - i) * qj * np.log2(qj) - \
-                (i-1)*(1./(2.*self.N))*np.log2(1./(2.*self.N))
-                Cmaxx[(i-1)*self.nsteps+l] = S / self.log2_N
-                Cmaxy[(i-1)*self.nsteps+l] = -2.*(S/self.log2_N)*(Scom - 0.5*S - 0.5*self.log2_N) \
-                /((1. + self.invN)*self.log2_Np1 - 2.*np.log2(2.*self.N) + self.log2_N)
-                
-        return Cminx, Cminy, Cmaxx, Cmaxy     
-
-    def getMaxC(self):
-        """
-        Get maximum possible Statistical Complexity value
-        from HC bounding curves for sampling size n
-
-        Returns
-        -------
-        float
-            Maximum possible C value for given n
-        """
-        Cminx, Cminy, Cmaxx, Cmaxy = self.Cmaxmin()
-        return np.max(Cmaxy)
 
     def getHC_boundary_lines(self, ax, lw=1., ls='--', color='k', alpha=0.5):
         """
@@ -138,9 +58,9 @@ class HCplots:
         alpha : float, optional
             Opacity of boundary lines (float between 0 and 1), by default 0.5
         """
-        minH, minC, maxH, maxC = self.Cmaxmin() # H and C values for HC min/max curves
-        minHVal = utils.HminPer() # Min. H value for periodic fnct
-        maxHVal = utils.HmaxPer() # Max. H value for periodic fnct
+        minH, minC, maxH, maxC = utils.Cmaxmin(n=self.n, nsteps=self.nsteps) # H and C values for HC min/max curves
+        minHVal = utils.HminPer(n=self.n) # Min. H value for periodic fnct
+        maxHVal = utils.HmaxPer(n=self.n) # Max. H value for periodic fnct
         argMinH_CminCurve = np.argmin(np.abs(minH-minHVal)) # Min. H value argument for H values for min HC curve
         argMinH_CmaxCurve = np.argmin(np.abs(maxH-minHVal)) # Min. H value argument for H values for max HC curve
         argMaxH_CminCurve = np.argmin(np.abs(minH-maxHVal)) # Max. H value argument for H values for min HC curve
@@ -177,7 +97,7 @@ class HCplots:
             Path to save plot if savePlot set to True, by default ''
             Note: Use only forward slashes in savePath
         """
-        Cminx, Cminy, Cmaxx, Cmaxy = self.Cmaxmin()
+        Cminx, Cminy, Cmaxx, Cmaxy = utils.Cmaxmin(n=self.n, nsteps=self.nsteps)
 
         ax.plot(Cminx,Cminy,'k-',Cmaxx,Cmaxy,'k-')
 
