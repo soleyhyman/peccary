2a8dd6d58fa4d4157dab4e19a8a6ea40adf14d8a
soleyhyman <soleyhyman@arizona.edu>
2024-07-14 00:05:33 +0200

    added version and author info

--- peccary/__init__.py
+++ peccary/__init__.py
@@ -4,6 +4,8 @@ The PECCARY package
 
 # explicitly set the package variable to ensure relative import work
 __package__ = "peccary"
+__version__ = "1.0.0"
+__author__ = "Sóley Hyman"
 
 __all__ = ["peccary","HCplots","timeseries","examples","utils"]
 
